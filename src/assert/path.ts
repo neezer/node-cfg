4f46fbe8f5469d68d6294bbeb03ec515f86b5bc3
Evan Sherwood <evan@sherwood.io>
2019-06-12 15:15:57 -0700

    fix: change path error message

--- src/assert/path.ts
+++ src/assert/path.ts
@@ -1,12 +1,16 @@
 import fs from "fs";
 import { Assert } from ".";
 
-export const assert: Assert = (_, value) => {
+export const assert: Assert = (keyPath, value) => {
   const exists = fs.existsSync(value);
 
   if (exists) {
     return [null, value];
   }
 
-  return [`no file present at ${value}`, false];
+  if (value === undefined) {
+    return [`for ${keyPath}, value is undefined`, false];
+  }
+
+  return [`for ${keyPath}, no file present at ${value}`, false];
 };
