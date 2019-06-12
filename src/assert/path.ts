2aac7db408e2855f705bedd747e2bb3f2fb26caf
Evan Sherwood <evan@sherwood.io>
2019-06-12 14:18:04 -0700

    chore: remove unused variable

--- src/assert/path.ts
+++ src/assert/path.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import { Assert } from ".";
 
-export const assert: Assert = (keyPath, value) => {
+export const assert: Assert = (_, value) => {
   const exists = fs.existsSync(value);
 
   if (exists) {
